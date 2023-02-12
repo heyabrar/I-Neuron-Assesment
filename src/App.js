05ac5ffb466c3acf1a8acb8d43d83c07233f6811
Abrar Khan <heyabrarkhan@gmail.com>
2023-02-12 12:21:04 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1 className="text-2xl font-bold underline text-red-600">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
